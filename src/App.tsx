413f61ddc392bac082c8207c3256fc0003675078
nabilpatel4012 <nabilpatel4012@gmail.com>
2023-07-23 15:44:10 +0530

    base project setup and cleanup

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import bgImage from '../src/assets/logo.svg';
+function App() {
+  
+
+  return (
+    <>
+     <h1>CRM + Ticketing Tool</h1>
+     <img src={bgImage} alt='Main Logo'></img>
+     <h3>Innovate, Elevate, Confide the Journey</h3>
+     <h4>Dummy h4</h4>
+    </>
+  )
+}
+
+export default App
